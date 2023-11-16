461bed70865b9756e1fe801fb20bef48487dd84d
Gregory El Bajoury <145674649+GregoryElBajoury@users.noreply.github.com>
2023-11-16 12:25:43 +0100

    Update variables.tf
    Add variables

--- variables.tf
+++ variables.tf
@@ -57,5 +57,50 @@ variable "identity_type" {
   default     = "SystemAssigned"
 }
 
-# ------------------- Fin variables azure_storage_sftp.tf ---------------------
+variable "key_vault_name" {
+  description = "Le nom du Key Vault à utiliser pour le déploiement des ressources"
+  type        = string
+}
 
+variable "tenant_id" {
+  description = "L'ID de locataire Azure Active Directory de votre abonnement Azure"
+  type        = string
+}
+
+variable "key_vault_sku_name" {
+  description = "Le niveau de service pour le Key Vault (standard ou premium)"
+  type        = string
+}
+
+variable "soft_delete_retention_days" {
+  description = "Le nombre de jours pendant lesquels le Key Vault conserve les éléments supprimés"
+  type        = number
+}
+
+variable "purge_protection_enabled" {
+  description = "Un booléen pour activer ou désactiver la protection contre la purge"
+  type        = bool
+}
+
+variable "network_acls_default_action" {
+  description = "L'action par défaut pour le contrôle d'accès réseau (Allow ou Deny)"
+  type        = string
+}
+
+variable "network_acls_bypass" {
+  description = "Définit quel trafic peut contourner les règles du réseau (par exemple, AzureServices)"
+  type        = string
+}
+
+variable "network_acls_ip_rules" {
+  description = "Liste des adresses IP ou plages d'IP autorisées à accéder au Key Vault"
+  type        = list(string)
+}
+
+variable "network_acls_virtual_network_subnet_ids" {
+  description = "Les ID des sous-réseaux du réseau virtuel autorisés à accéder au Key Vault"
+  type        = list(string)
+}
+
+
+# ------------------- Fin variables azure_storage_sftp.tf ---------------------
